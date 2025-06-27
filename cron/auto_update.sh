37c9c29a39abf3579a6cf7f291c0045e093e62b6
MUSIAL Paul <paul.musial@telecomnancy.net>
2025-06-27 18:13:53 -0400

    typo correction

--- cron/auto_update.sh
+++ cron/auto_update.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# Script to fetch all timetables
+
 declare -a groups=("fise_1a_g1"
   "fise_1a_g2"
   "fise_1a_g3"
   "fise_1a_g4"
 )
 
-rm -f timetables/*.ics
+rm -f /path/to/repo/timetables/*.ics
 
 for group in "${groups[@]}"
 do
-    curl "https://edt.telecomnancy.univ-lorraine.fr/static/${group}.ics" >> "timetables/${group}.ics"
+    curl "https://edt.telecomnancy.univ-lorraine.fr/static/${group}.ics" >> "/path/to/repo/timetables/${group}.ics"
 done
--- cron/test.sh
+++ cron/test.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "$(date)" >> /home/paul/Documents/artebot/cron/log.txt
+# Script to test cron automated task
+# Writes the current date and time in a new line in log.txt
+
+echo "$(date)" >> /path/to/repo/cron/log.txt
