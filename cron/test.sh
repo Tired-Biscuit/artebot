f3584eec9c6f1bd1e2d991688799ec4779bd303c
MUSIAL Paul <paul.musial@telecomnancy.net>
2025-06-27 18:23:33 -0400

    Better scripts with relative path

--- cron/test.sh
+++ cron/test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # Script to test cron automated task
-# Writes the current date and time in a new line in log.txt
+# Writes the current date and time in a new line in logs.txt
+# Add the worker with this line for a timestamp each minute
+#
+# * 6 * * * cd /path/to/repo/ && ./cron/test.sh
+#
 
-echo "$(date)" >> /path/to/repo/cron/log.txt
+echo "$(date)" >> ./cron/logs.txt
