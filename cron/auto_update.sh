7cd77bcc4d1a911d94ca92035091fa43aa0a595c
Paul MUSIAL <paul.musial@telecomnancy.net>
2025-06-27 17:43:30 -0400

    Added folders for project architecture, and script to fetch timetables from school website

---
+++ cron/auto_update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+declare -a groups=("fise_1a_g1"
+  "fise_1a_g2"
+  "fise_1a_g3"
+  "fise_1a_g4"
+)
+
+rm -f timetables/*.ics
+
+for group in "${groups[@]}"
+do
+    curl "https://edt.telecomnancy.univ-lorraine.fr/static/${group}.ics" >> "timetables/${group}.ics"
+done
