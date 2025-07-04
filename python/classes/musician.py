496bd064e7a0d7dbc0ca3d3e457b509e81570f80
Paul MUSIAL <paul.musial@telecomnancy.net>
2025-07-04 17:09:16 +0200

    Richer error message for db.run method
    Added the possibility to instanciate Musician from uuid (to be removed later)
    
    Added a test file, which will eventually be switched to a pytest-compliant one

--- python/classes/musician.py
+++ python/classes/musician.py
@@ -3,23 +3,47 @@ import python.db as db
 
 class Musician:
     """
-    Represents a musician with a list of events.
+    Represents a musician with a list of its constraints as Event.
+
+    Instanciate from either email address or uuid
     """
 
-    def __init__(self, mail: str):
+    def __init__(self, *, mail: str = "", id: str = ""):
+        self.id: str
+        self.mail: str
+        self.events: list[Event]
+        self.recurring_events: list[RecurringEvent]
+        self.group: str
 
-        id = db.run(f"SELECT uuid FROM User WHERE email = '{mail}'")
+        if (mail != "" and id == ""):
+            id = db.run(f"SELECT uuid FROM User WHERE email = '{mail}'")
 
-        if not id:
-            raise ValueError(f"User with email {mail} does not exist.")
-        
-        self.id = id[0][0]
+            if not id:
+                raise ValueError(f"User with email {mail} does not exist.")
 
-        events = db.run(f"SELECT * FROM MusicianConstraint WHERE musician = '{self.id}' AND week_day = 0;")
+            self.mail = mail
+            self.id = id[0][0]
 
-        recurring_events = db.run(f"SELECT * FROM MusicianConstraint WHERE musician = '{self.id}' AND week_day != 0;")
+        elif (mail == "" and id != ""):
+            mail = db.run(f"SELECT email FROM User WHERE uuid = '{id}'")
+
+            if not mail:
+                raise ValueError(f"User with uuid {id} does not exist.")
+
+            self.mail = mail[0][0]
+            self.id = id
+
+        else:
+            raise ValueError("Either multiple or no parameters were given, or they were null")
+
+        events = db.run(f"SELECT * FROM MusicianConstraint WHERE musician_uuid = '{self.id}' AND week_day = 0;")
+
+        recurring_events = db.run(f"SELECT * FROM MusicianConstraint WHERE musician_uuid = '{self.id}' AND week_day != 0;")
 
         self.events = constraints_to_events(events)
         self.recurring_events = recurring_constraints_to_events(recurring_events)
 
-        self.group = db.run(f"SELECT group_id FROM User WHERE email = '{mail}'")[0][0]
+        group = db.run(f"SELECT group_id FROM User WHERE email = '{self.mail}'")
+        if not group:
+            raise Exception(f"Could not find group id for user with email '{self.mail}' and uuid '{self.id}'")
+        self.group = group[0][0]
