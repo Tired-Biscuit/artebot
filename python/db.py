b09028e46dbdb6ace2c01d93d427e4dc30fef65e
LOISIL Tom <66916111+Tomygood@users.noreply.github.com>
2025-08-27 15:50:10 +0200

    regex fix (miaoût)

--- python/db.py
+++ python/db.py
@@ -251,7 +251,8 @@ def add_rehearsal_to_calendar(song:str, attendees:list[str], creator:str, start_
         "organizer": {"email": song_info[4]},
         "guestsCanModify": True
     }
-    return googleutils.add_event_to_calendar(googleutils.get_calendar_id(tools.get_setlist_calendar_url(song_info[0])), event)
+    
+    return googleutils.add_event_to_calendar(song_info[0], event)
 
 def add_user(uuid, username, email, group_id, *, commit=False):
     """
