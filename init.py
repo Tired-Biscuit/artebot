715ff5cfc81bc8ead4c78f953def7570c6ce8602
Paul MUSIAL <paul.musial@telecomnancy.net>
2025-08-27 21:18:59 +0200

    Better docker things

--- init.py
+++ init.py
@@ -6,4 +6,4 @@ if len(sys.argv) > 1:
     tools.add_owner(int(sys.argv[1]))
     print(f"Added {sys.argv[1]} as owner")
     tools.add_admin(int(sys.argv[1]))
-    print(f"Added {sys.argv[1]} as owner")
+    print(f"Added {sys.argv[1]} as admin")
